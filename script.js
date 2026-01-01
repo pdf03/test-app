6ef3e93458833790da459f569f4694f599ec778e
pdf03 <falldoudou87@gmail.com>
2026-01-01 02:36:50 +0000

    add getTest

--- script.js
+++ script.js
@@ -3,5 +3,11 @@ console.log("Hey");
 const test = document.querySelector("test_app")
 
 test.addEventListener ("click" , () => {
-    document.querySelector("#result").textContent = Math.random();
-})
+    document.querySelector("#result").textContent = Math.floor(
+        getTest(1,100)
+    )
+});
+
+function getTest(min,max) {
+    return Math.random = (max - min) + min;
+}
